6634507143ab4f70cfebc074eb4e02ac91b4ffff
Alan Wang <alanwang123919@gmail.com>
2024-02-15 17:06:52 -0800

    created hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import { Box, Text } from '@chakra-ui/react'
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <Box>
+        <Text>
+            Hi there! I'm Alan!
+        </Text>
+        <Text>
+            I'm a computer science + biochemistry student 
+        </Text>
+
+        <Text>
+            SCROLL FOR MORE
+        </Text>
+    </Box>
+  )
+}
+
+export default Hero
