99e6591ba8018d57160d8bb49b8552151faac498
Alan Wang <alanwang123919@gmail.com>
2024-02-15 17:07:25 -0800

    created projects

---
+++ src/components/Projects.jsx
@@ -0,0 +1,14 @@
+import { Box, Text } from '@chakra-ui/react'
+import React from 'react'
+
+const Projects = () => {
+  return (
+    <Box>
+        <Text>
+            Projects
+        </Text>
+    </Box>
+  )
+}
+
+export default Projects
