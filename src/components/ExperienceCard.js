64c9767c9b8f5c9bfeb5b3b644ec4cd8020f38c9
Alan Wang <alanwang123919@gmail.com>
2024-02-15 22:57:36 -0800

    updated experiences list to include image and company name

--- src/components/ExperienceCard.js
+++ src/components/ExperienceCard.js
@@ -1,10 +1,32 @@
+import { Box, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 
-const ExperienceCard = () => {
+const ExperienceCard = ({image, title, company, location, points}) => {
   return (
-    <div>
-      
-    </div>
+    <Box className='experience-card-container'>
+
+      <Box className='experience-card-title-section'>
+        <Image src={image} alt='hi'/>
+        <Text className='job-title'>
+          {title}
+        </Text>
+        <Text className='job-company'>
+          {company}
+        </Text>
+      </Box>
+
+      <Text className='job-location'>
+        {location}
+      </Text>
+
+      {points.map((point, index) => {
+        return (
+          <Text key={index} className='job-point'>
+            • {point}
+          </Text>
+        )
+      })}
+    </Box>
   )
 }
 
