ee73b39336f049d6bcdf6a42e0ca8fbd3e0fe5c2
Alan Wang <alanwang123919@gmail.com>
2024-02-15 17:08:06 -0800

    changes to app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
+import Hero from './components/Hero';
+import AboutMe from './components/AboutMe';
+import Experiences from './components/Experiences';
+import Projects from './components/Projects';
+import Ending from './components/Ending';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <Hero/>
+        <AboutMe/>
+        <Experiences/>
+        <Projects/>
+        <Ending/>
+      </div>
+    </ChakraProvider>
   );
 }
 
