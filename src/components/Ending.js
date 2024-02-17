67dcdfc9ad7255f7cc3bb106b337c25a6ead9023
Alan Wang <alanwang123919@gmail.com>
2024-02-17 00:08:32 -0800

    added social media icons

--- src/components/Ending.js
+++ src/components/Ending.js
@@ -1,6 +1,26 @@
 import { Box, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 import Footer from './Footer'
+import { SocialIcon } from 'react-social-icons'
+
+const socials = [
+  {
+    link: "https://www.linkedin.com/in/alanwang093",
+    photo: "www.linkedin.com",
+  },
+  {
+    link: "https://www.instagram.com/alanwang123919/",
+    photo: "www.instagram.com",
+  },
+  {
+    link: "https://www.youtube.com/@alanwang093/featured",
+    photo: "www.youtube.com",
+  },
+  {
+    link: "https://github.com/A1aNNN",
+    photo: "www.github.com",
+  },
+]
 
 const Ending = () => {
   return (
@@ -20,38 +40,20 @@ const Ending = () => {
             Thanks for stopping by!
           </Text>
 
-          <Box display='flex' flexDirection='row'>
-            <Image alt='LinkedIn'/>
-            <Image alt='Instagram'/>
-            <Image alt='Email'/>
-            <Image alt='YouTube'/>
-            <Image alt='GitHub'/>
+          <Box className='ending-socials'>
+            {socials.map((social, index) => {
+              return (
+                <a href={social.link} target='_blank'>
+                  <SocialIcon key={index} url={social.photo} bgColor='white' fgColor='#BAD6FB'
+                    className='social'
+                  />
+                </a>
+              )
+            })}
           </Box>
 
           <Footer/>
         </Box>
-        {/* <Text className='ending-title'>
-            That's all for now!
-        </Text>
-
-        <Text className='ending-text'>
-            I truly appreciate you stopping by to explore my little corner of the internet. I'm always eager to connect, exchange ideas, 
-            or simply have a friendly chat, so feel free to reach out through any of my social media platforms listed below or 
-            send me an email for a more personal touch!
-            <br/>
-            <br/>
-            Thanks for stopping by!
-        </Text>
-
-        <Box display='flex' flexDirection='row'>
-            <Image alt='LinkedIn'/>
-            <Image alt='Instagram'/>
-            <Image alt='Email'/>
-            <Image alt='YouTube'/>
-            <Image alt='GitHub'/>
-        </Box>
-
-        <Footer/> */}
       </Box>
     </Box>
   )
