d79159b5b10ed168c9d3ac3a2a2aa8d9e95b0dba
Alan Wang <alanwang123919@gmail.com>
2024-02-17 01:05:41 -0800

    UPDATE: got square to appear in background

--- src/App.js
+++ src/App.js
@@ -6,19 +6,36 @@ import AboutMe from './components/AboutMe';
 import Experiences from './components/Experiences';
 import Projects from './components/Projects';
 import Ending from './components/Ending';
-import { RoundedBox } from '@react-three/drei';
+import { RoundedBox, Scroll, ScrollControls } from '@react-three/drei';
 
 function App() {
   return (
-    <ChakraProvider>
-      <div className="App">
-        <Hero/>
-        <AboutMe/>
-        <Experiences/>
-        <Projects/>
-        <Ending/>
-      </div>
-    </ChakraProvider>
+    <>
+      <ScrollControls pages={6.15}>
+
+        <RoundedBox
+          args={[1,1,1]}
+          radius={0.05}
+          smoothness={4}
+        >
+          <meshBasicMaterial/>
+        </RoundedBox>
+
+        <Scroll></Scroll>
+
+        <Scroll html style={{width: '100%'}}>
+          <ChakraProvider>
+            <div className="App">
+              <Hero/>
+              <AboutMe/>
+              <Experiences/>
+              <Projects/>
+              <Ending/>
+            </div>
+          </ChakraProvider>
+        </Scroll>
+      </ScrollControls>
+    </>
   );
 }
 
