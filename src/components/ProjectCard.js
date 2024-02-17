7fe517bf6222c84206e3742b66ea1bc56631ae86
Alan Wang <alanwang123919@gmail.com>
2024-02-16 23:19:16 -0800

    created projects section

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,10 +1,47 @@
+import { Box, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 
-const ProjectCard = () => {
+const ProjectCard = ({link, image, title, date, points, tech}) => {
   return (
-    <div>
-      
-    </div>
+    <Box className='project-card-container'>
+      <Box className='project-card-main'>
+
+        <Box className='project-image-container'>
+          <a href={link} target='_blank'>
+            <Image src={image} className='project-image'/>
+          </a>
+        </Box>
+
+        <Box className='project-card-main-right'>
+          <Text className='project-title'>
+            {title}
+          </Text>
+
+          <Text className='project-date'>
+            {date}
+          </Text>
+
+          {points.map((point, index) => {
+            return (
+              <Text key={index} className='project-point'>
+                • {point}
+              </Text>
+            )
+          })}
+        </Box>
+      </Box>
+
+      <Box className='project-tech-stack'>
+          {tech.map((t, index) => {
+            return (
+              <Text key={index} className='project-tech'>
+                {t}
+              </Text>
+            )
+          })}
+        </Box>
+
+    </Box>
   )
 }
 
