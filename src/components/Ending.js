fae25cc149649deef5820b94dc405a45964c4caf
Alan Wang <alanwang123919@gmail.com>
2024-02-16 23:45:54 -0800

    some styling for ending section

--- src/components/Ending.js
+++ src/components/Ending.js
@@ -4,16 +4,42 @@ import Footer from './Footer'
 
 const Ending = () => {
   return (
-    <Box>
-        <Text>
+    <Box className='ending'>
+      <Box className='ending-container'>
+        <Box className='ending-width-85'>
+          <Text className='ending-title'>
             That's all for now!
-        </Text>
+          </Text>
 
-        <Text>
+          <Text className='ending-text'>
             I truly appreciate you stopping by to explore my little corner of the internet. I'm always eager to connect, exchange ideas, 
             or simply have a friendly chat, so feel free to reach out through any of my social media platforms listed below or 
             send me an email for a more personal touch!
+            <br/>
+            <br/>
+            Thanks for stopping by!
+          </Text>
 
+          <Box display='flex' flexDirection='row'>
+            <Image alt='LinkedIn'/>
+            <Image alt='Instagram'/>
+            <Image alt='Email'/>
+            <Image alt='YouTube'/>
+            <Image alt='GitHub'/>
+          </Box>
+
+          <Footer/>
+        </Box>
+        {/* <Text className='ending-title'>
+            That's all for now!
+        </Text>
+
+        <Text className='ending-text'>
+            I truly appreciate you stopping by to explore my little corner of the internet. I'm always eager to connect, exchange ideas, 
+            or simply have a friendly chat, so feel free to reach out through any of my social media platforms listed below or 
+            send me an email for a more personal touch!
+            <br/>
+            <br/>
             Thanks for stopping by!
         </Text>
 
@@ -25,7 +51,8 @@ const Ending = () => {
             <Image alt='GitHub'/>
         </Box>
 
-        <Footer/>
+        <Footer/> */}
+      </Box>
     </Box>
   )
 }
