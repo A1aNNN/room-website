02de5f4661b1633d3f5e4e2590606989a0ce0bd5
Alan Wang <alanwang123919@gmail.com>
2024-03-03 00:08:15 -0800

    hero section styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,16 +6,17 @@ import WordsTypewriter from './WordsTypewriter'
 const Hero = () => {
   return (
     <Box className='hero-container'>
+      <Box className='hero-intro-container'>
         <Text className='hero-title'>
             👋 Hi there! I'm Alan!
         </Text>
         <Box className='hero-typewriter-container'>
           <Text className='hero-typewriter'>
-              {/* I'm a computer science + biochemistry student  */}
               I'm a 
           </Text>
           <WordsTypewriter/>
         </Box>
+      </Box>
 
         <Text className='scroll-for-more'>
             SCROLL FOR MORE
