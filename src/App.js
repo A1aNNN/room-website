85a94b04fd7af0e6fb7771ad902e26503342046c
Alan Wang <alanwang123919@gmail.com>
2024-05-25 11:58:31 -0700

    added fade in / fade out effect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Fade } from '@chakra-ui/react'
 import Hero from './components/Hero';
 import AboutMe from './components/AboutMe';
 import Experiences from './components/Experiences';
@@ -11,17 +11,29 @@ import { Scroll, ScrollControls } from '@react-three/drei';
 // import ToonRoomFaster from './components/ToonRoomFaster.js'
 import SplineRoom from './components/SplineRoom'
 import Fun from './components/Fun';
+import useOnScreen from './components/useOnScreen';
+import { useRef } from 'react';
 
+const Section = ({ children }) => {
+  const ref = useRef();
+  const isVisible = useOnScreen(ref, "-25% 0px -25% 0px");
+
+  return (
+    <div ref={ref}>
+      <Fade in={isVisible} transition={{ enter: { duration: 1 }, exit: { duration: 1 } }}>
+        <div>
+          {children}
+        </div>
+      </Fade>
+    </div>
+  );
+};
 
 function App() {
   return (
     <>
       <ScrollControls pages={8.48}>
 
-        
-        {/* <TableSpongebob/> */}
-        {/* <ToonRoom/> */}
-        {/* <ToonRoomFaster/> */}
         <SplineRoom scale={0.12}/>
 
         <Scroll></Scroll>
@@ -29,12 +41,30 @@ function App() {
         <Scroll html style={{width: '100%'}}>
           <ChakraProvider>
             <div className="App">
-              <Hero/>
-              <AboutMe/>
-              <Experiences/>
-              <Projects/>
-              <Fun/>
-              <Ending/>
+              
+              <Section>
+                <Hero/>
+              </Section>
+              
+              <Section>
+                <AboutMe/>
+              </Section>
+              
+              <Section>
+                <Experiences/>
+              </Section>
+
+              <Section>
+                <Projects/>
+              </Section>
+
+              <Section>
+                <Fun/>
+              </Section>
+
+              <Section>
+                <Ending/>
+              </Section>
             </div>
           </ChakraProvider>
         </Scroll>
